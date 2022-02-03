29dfaad5072b403983b1500bf79dc49a8cadcb2e
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-03 19:31:40 +0100

    Added basic structure of a form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
+import Button from "react-bootstrap/Button";
+import bootstrap from "bootstrap";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <header className="d-flex align-items-center justify-content-center">
+        <h1>FORM</h1>
+      </header>
+
+      <main>
+        <section className="d-flex align-items-center justify-content-center">
+          <form action="" className="row g-3 w-50 p-3 ">
+            <div className="col-md-6 ">
+              <label forhtml="inputEmail3" class="form-label">
+                Name:
+              </label>
+              <input type="text" className="form-control" />
+            </div>
+
+            <div className="col-md-6">
+              <label forhtml="inputEmail3" class="form-label">
+                Last name:
+              </label>
+              <input type="text" className="form-control" />
+            </div>
+
+            <div className="col-md-6">
+              <label forhtml="inputEmail3" class="form-label">
+                Birthdate:
+              </label>
+              <input type="text" className="form-control" />
+            </div>
+
+            <div className="col-md-6">
+              <label forhtml="inputEmail3" class="form-label">
+                Email:
+              </label>
+              <input type="text" className="form-control" />
+            </div>
+
+            <div className="d-flex align-items-center justify-content-center">
+              <button className="btn btn-primary">Anterior</button>
+              <button className="btn btn-primary">Siguiente</button>
+            </div>
+          </form>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
